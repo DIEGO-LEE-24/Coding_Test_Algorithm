d062a046f7e8f6bc2afb86dec1e9e5c33c5352a8
Diego Lee Sang Cheol <diegolee5494@gmail.com>
2024-08-19 03:39:46 -0600

    Update 백준10828.py

--- 밀라/백준10828.py
+++ 밀라/백준10828.py
@@ -9,16 +9,17 @@ input = sys.stdin.readline
 
 stack = []
 n = int(input())
-for_in range(n):
-    quy = input().split()
+
+for _ in range(n):
+    qry = input().strip().split()
     if qry[0] == 'push':
         stack.append(qry[1])
     elif qry[0] == 'pop':
         print(stack.pop() if stack else -1)
     elif qry[0] == 'size':
         print(len(stack))
-    elif qry[0] == "empty":
+    elif qry[0] == 'empty':
         print(0 if stack else 1)
-    else:
+    elif qry[0] == 'top':
         print(stack[-1] if stack else -1)
         
