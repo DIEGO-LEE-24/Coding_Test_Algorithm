8b1c75a78a2bce505a5eb87441132333c77ddea8
Diego Lee Sang Cheol <diegolee5494@gmail.com>
2024-08-19 03:30:20 -0600

    Update 백준10828.py

--- 밀라/백준10828.py
+++ 밀라/백준10828.py
@@ -1,35 +1,24 @@
+# 리스트
+# push append
+# pop pop
+# size len
+# empty if stack:
+# top stack[-1]
 import sys
-input = sys.stdin.read
+input = sys.stdin.readline
 
-def main():
-    data = input().strip().split('\n')
-    N = int(data[0])
-    commands = data[1:]
-
-    stack = []
-    results = []
-    
-    for command in commands:
-        if command.startswith("push"):
-            _, x = command.split()
-            stack.append(int(x))
-        elif command == "pop":
-            if stack:
-                results.append(stack.pop())
-            else:
-                results.append(-1)
-        elif command == "size":
-            results.append(len(stack))
-        elif command == "empty":
-            results.append(1 if not stack else 0)
-        elif command == "top":
-            if stack:
-                results.append(stack[-1])
-            else:
-                results.append(-1)
-
-    # Print all results
-    sys.stdout.write("\n".join(map(str, results)) + "\n")
-
-if __name__ == "__main__":
-    main()
+stack = []
+n = int(input())
+for_in range(n):
+    quy = input().split()
+    if qry[0] == 'push':
+        stack.append(qry[1])
+    elif qry[0] == 'pop':
+        print(stack.pop() if stack else -1)
+    elif qry[0] == 'size':
+        print(len(stack))
+    elif qry[0] == "empty":
+        print(0 if stack else 1)
+    else:
+        print(stack[-1] if stack else -1)
+        
